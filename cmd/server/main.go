128384d4c5a4da6ef14775fd19c39f39f49061f7
gvs89 <112775497+gvs89@users.noreply.github.com>
2025-01-15 23:29:00 +0300

    v2

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,15 @@
-package server
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/yourusername/yourprojectname/internal/api"
+)
+
+func main() {
+	router := api.Router()
+
+	log.Println("Запуск сервера...")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
--- internal/pdf/generator.go
+++ internal/pdf/generator.go
@@ -1,1 +1,33 @@
+// internal/pdf/generator.go
 package pdf
+
+import (
+	"fmt"
+	"github.com/jung-kurt/gofpdf"
+	"log"
+)
+
+type EstimateRequest struct {
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
+
+func pdfGenerator(filename string, estimate EstimateRequest) {
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf.AddPage()
+	pdf.SetFont("Arial", "", 16)
+	pdf.Cell(40, 10, estimate.Title)
+	pdf.Ln(-1)
+	pdf.SetFont("", "", 12)
+	pdf.Write(8, estimate.Content)
+
+	err := pdf.OutputFileAndClose(filename)
+	if err != nil {
+		log.Fatalf("Ошибка при сохранении файла: %v\n", err)
+	}
+	fmt.Printf("PDF файл сохранён: %s\n", filename)
+}
+func sendToWhatsApp(fileName string) {
+	// Здесь будет логика для отправки файла через WhatsApp
+	// Например, использование WhatsApp Business API
+}
